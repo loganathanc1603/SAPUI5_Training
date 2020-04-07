9a19bfffc2497224de35e68d83e1bfc16f00d1f9
loganathanc1603 <loganathan.chinnasaamy@gmail.com>
2020-04-07 12:27:05 +0000

    important comments are maintained each file

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -5,13 +5,18 @@ sap.ui.define([
 
 	return Controller.extend("com.ui5.SAPUI5_Session.controller.MainView", {
 		onInit: function () {
+			//Creating a jsonmodel
 			var jmodel = new sap.ui.model.json.JSONModel();
+			//creating a object with default values
 			var obj = {
 				"FName": "Test",
 				"LName": "TestName"
 			};
+			//setting default binding mode "ONeWay"
 			jmodel.setDefaultBindingMode("OneWay");
+			//Setting the data to jsonmodel using setData() method
 			jmodel.setData(obj);
+			//Setting the model to current view using setModel() method
 			this.getView().setModel(jmodel);
 		}
 	});
