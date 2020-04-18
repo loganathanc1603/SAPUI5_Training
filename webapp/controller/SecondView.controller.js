7d03c7c26178ce9b90aae100576a913922e50783
loganathanc1603 <loganathan.chinnasaamy@gmail.com>
2020-04-18 15:05:39 +0000

    18.04.2020 Session Coding

--- webapp/controller/SecondView.controller.js
+++ webapp/controller/SecondView.controller.js
@@ -1,12 +1,13 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"com/ui5/SAPUI5_Session/controller/BaseController",
-	"sap/ui/core/routing/History"
-], function (Controller, BaseController, History) {
+	"sap/ui/core/routing/History",
+	"com/ui5/SAPUI5_Session/model/formatter"
+], function (Controller, BaseController, History, formatter) {
 	"use strict";
 
 	return BaseController.extend("com.ui5.SAPUI5_Session.controller.SecondView", {
-
+		formatter: formatter,
 		onInit: function () {
 			this.getRouter().getRoute("SecondView").attachPatternMatched(this.onObjectMatched, this);
 		},
