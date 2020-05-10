6a263e5b6872d8380d391dcb127d85d0a2b09037
loganathanc1603 <loganathan.chinnasaamy@gmail.com>
2020-05-10 13:58:37 +0000

    08.05.2020 SAPUI5 Session [ Smart Controls ]

---
+++ webapp/controller/SmartControls.controller.js
@@ -0,0 +1,26 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"com/ui5/SAPUI5_Session/model/formatter"
+], function (Controller, formatter) {
+	"use strict";
+
+	return Controller.extend("com.ui5.SAPUI5_Session.controller.SmartControls", {
+		formatter: formatter,
+	
+		onInit: function () {
+
+		},
+		
+		fnOnBeforeRebind: function(evt){
+			var Obj = evt;
+		},
+		
+		onPrsNavBusi: function(evt){
+			var sPath = evt.getSource().getBindingContextPath();
+			this.getView().byId("smartFormId").bindElement(sPath);
+		}
+
+
+	});
+
+});
